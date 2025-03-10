3dc5b06fd07c1ecebcf05c15c9eda2d6b33ee9f8
dimple-02 <dimple1080.becse24@chitkara.edu.in>
2025-03-10 16:50:45 +0530

    Solution to Check for Palindrome Array

--- Check for Palindrome Array@@676c02002dbc8c3e7a280b7a/code.c
+++ Check for Palindrome Array@@676c02002dbc8c3e7a280b7a/code.c
@@ -9,7 +9,7 @@ int main(){
         int count=0;
         for(int i=0;i<n/2;i++){
             if(arr[i]==arr[n-1-i]){
-                count++
+                count++;
             }
         } 
     if(count==n/2){
