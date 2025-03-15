4e5a0062a6fecd638da085e34c9207accbab4373
dimple-02 <dimple1080.becse24@chitkara.edu.in>
2025-03-15 23:46:16 +0530

    Solution to Check if a String is a Palindrome After Removing Spaces

--- Check if a String is a Palindrome After Removing Spaces@@67877c59ddaeab38c8ad25b7/code.c
+++ Check if a String is a Palindrome After Removing Spaces@@67877c59ddaeab38c8ad25b7/code.c
@@ -15,7 +15,7 @@ int main(){
    int len=strlen(st);
    char st2[50];
    for(int i=0;i<len;i++){
-    st[i]=st[len-i-1];
+    st[i]=st2[len-i-1];
    }
    if (strcmp(st,st2)){
     printf("Yes");
